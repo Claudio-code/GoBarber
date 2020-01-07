6075edf27339645f60d5d0c16372a78a60c7f85f
claudio2424 <suporte2424@outlook.com>
2020-01-07 01:22:08 -0300

    configurado o layout

--- front_end/src/pages/SignUp/index.js
+++ front_end/src/pages/SignUp/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 // import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <div>SignUp</div>
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/" >Já tenho conta</Link>
+      </Form>
+    </>
   );
 }
