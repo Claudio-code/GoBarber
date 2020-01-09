38b20fb2f5555f61da34d7f8affb98f4a5effcf5
claudio-code <suporte2424@outlook.com>
2020-01-09 07:31:53 -0300

    configurado o reducer do usuario

--- front_end/src/store/modules/rootReducer.js
+++ front_end/src/store/modules/rootReducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import auth from './auth/reducer';
 
+import user from './user/reducer';
+
 export default combineReducers({
-    auth,
+    auth, user
 });
