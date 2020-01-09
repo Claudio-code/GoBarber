b12ee11569e65349e43c4a04e1590d858c1c6f85
claudio-code <suporte2424@outlook.com>
2020-01-09 07:20:13 -0300

    configurado altenticacao

--- front_end/src/pages/SignIn/index.js
+++ front_end/src/pages/SignIn/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'; 
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -14,9 +17,11 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+    
   }
 
   return (
--- front_end/src/store/modules/auth/saga.js
+++ front_end/src/store/modules/auth/saga.js
@@ -1,3 +1,33 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, put, call } from 'redux-saga/effects';
 
-export default all([]);
+import api from '../../../services/api';
+import history from '../../../services/history';
+
+import { signInSucess } from './actions';
+
+export function* signIn({ payload }) {
+try {
+    const { email, password } = payload;
+    const response = yield call(api.post, 'sessions', {
+        email, password
+    });
+    console.log(response.data);
+    
+    const { token, User } = response.data;
+
+    if (!User.provider) {
+        console.tron.error('usuario n permitido');
+        return;
+    }
+    yield put(signInSucess(token, User));
+
+    history.push('/dashboard');
+} catch (error) {
+    console.log(error);
+    return;
+}
+}
+
+export default all([
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
