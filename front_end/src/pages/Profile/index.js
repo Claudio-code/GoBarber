be6de594224ce9760ef28673026372b086f33fb0
claudio-code <suporte2424@outlook.com>
2020-01-13 03:12:58 -0300

    configurada a pagina de atualizacao de perfil

--- front_end/src/pages/Profile/index.js
+++ front_end/src/pages/Profile/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
-    <div>Profile</div>
+    <Container>
+      <Form initialData={profile}>
+        <Input 
+          name="name" 
+          placeholder="Digite seu nome completo" 
+        />
+        <Input 
+          name="email" 
+          placeholder="Digite seu nome completo" 
+        />
+        <hr/>
+
+        <Input  
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input  
+          type="password"
+          name="password"
+          placeholder="Sua senha nova"
+        />
+        <Input  
+          type="password"
+          name="confirmPassword"
+          placeholder="confirme sua senha nova"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair da aplicação</button>
+    </Container>
   );
 }
