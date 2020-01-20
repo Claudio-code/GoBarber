876171b21b68bf5a0bebc3ac942508e7a2c2f834
claudio-code <suporte2424@outlook.com>
2020-01-20 01:01:03 -0300

    configurado o estilo do dashboard

--- front_end/src/pages/Dashboard/index.js
+++ front_end/src/pages/Dashboard/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={35} color="#FFF" />
+        </button>
+        <strong> 4 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={35} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>09:00</strong>
+          <span>Kraudio</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Kraudio</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Kraudio</span>
+        </Time>
+        <Time>
+          <strong>12:00</strong>
+          <span>Kraudio</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
