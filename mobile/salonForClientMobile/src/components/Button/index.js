6ad58035ce646c7a9bca46caa036918a63a7d343
claudio-code <suporte2424@outlook.com>
2020-01-31 22:01:27 -0300

    configured style buttons and inputs

---
+++ mobile/salonForClientMobile/src/components/Button/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { Container, TextButton } from './styles';
+
+export default function Button({ children, loading, ...rest }) {
+
+  return (
+    <Container {...rest}>
+      { loading ? (
+          <ActivityIndicator size="small" color="#fff" />
+      ) : (
+        <TextButton>{children}</TextButton>
+      )}  
+    </Container>
+  );
+}
+
+Button.prototype = {
+  children: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  loading: false,
+};
--- mobile/salonForClientMobile/src/pages/SignIn/index.js
+++ mobile/salonForClientMobile/src/pages/SignIn/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+
 import Background from '../../components/background';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 export default function SignIn() {
   return (
-    <Background><Text>dwd</Text></Background>
+    <Background>
+      <Text>SignIn</Text>
+      <Input 
+        style={{ marginTop: 30 }}
+        icon="call" 
+        placeholder="Digite seu nome" 
+      />
+      <Button> Login </Button>
+    </Background>
   );
 }
