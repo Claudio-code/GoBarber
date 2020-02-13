d5e1ac700824f08a8faeb260a4918d5bee8c3f82
claudio-code <suporte2424@outlook.com>
2020-02-13 01:59:56 -0300

    configured route for create new appointment

---
+++ mobile/salonForClientMobile/src/pages/New/SelectProvider/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function SelectProvider() {
+  return (
+    <View>
+      <Text>SelectProvider</Text>
+    </View>
+  );
+}
--- mobile/salonForClientMobile/src/routes.js
+++ mobile/salonForClientMobile/src/routes.js
@@ -1,10 +1,15 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+
+import SelectProvider from './pages/New/SelectProvider';
+import Confirm from './pages/New/Confirm';
+import SelectDateTime from './pages/New/SelectDateTime';
 
 export default (signedIn = false) => 
   createAppContainer(
@@ -15,6 +20,14 @@ export default (signedIn = false) =>
       }),
       App: createBottomTabNavigator({
         Dashboard,
+        New: {
+          screen: createStackNavigator({
+            SelectProvider,
+            SelectDateTime,
+            Confirm
+          }),
+          
+        },
         Profile
       }, {
         tabBarOptions: {
