0479f050bf37a11d1f5228b1c2b56d646bd5a4a5
claudio-code <suporte2424@outlook.com>
2020-02-20 00:10:53 -0300

    configured date piker navigate form page confirm in mobile

--- mobile/salonForClientMobile/src/pages/New/SelectDateTime/index.js
+++ mobile/salonForClientMobile/src/pages/New/SelectDateTime/index.js
@@ -1,18 +1,67 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../../components/background';
 import DateInput from '../../../components/DateInput';
+import api from '../../../services/api';
 
-// import { Container } from './styles';
+import { Container, Hour, HoursList, Title } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadDate() {
+      try {
+        
+        const response = await api.get(`/providers/${provider.id}/available`, {
+          params: {
+            date: date.getTime(),
+          }
+        });
+        
+        setHours(response.data);
+
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    }
+    loadDate();
+  }, [date, provider.id]);
+
+  function handleSelectHours(value) {
+    navigation.navigate('Confirm', {
+      provider,
+      time: value
+    });
+  }
 
   return (
     <Background>
-      <DateInput date={date} onChange={setDate} />
+      <Container>
+        <DateInput 
+          date={date} 
+          onChange={setDate} 
+        />
+        <HoursList 
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour 
+              onPress={() => handleSelectHours(item.value)}
+              enabled={item.avaliable}
+            >
+              <Title>
+                {item.time}
+              </Title>
+            </Hour>
+          )}
+        />
+      </Container>
     </Background>
   );
 }
