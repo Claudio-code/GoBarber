554bcedd830455357eace2df4b7992237fc1089c
claudio-code <suporte2424@outlook.com>
2020-02-24 06:05:00 -0300

    create all functions and setting styles for is page

--- mobile/salonForClientMobile/src/pages/New/Confirm/index.js
+++ mobile/salonForClientMobile/src/pages/New/Confirm/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useMemo } from 'react';
+import pt from 'date-fns/locale/pt';
+import { parseISO, formatRelative } from 'date-fns';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '../../../components/background';
+import { Container, Avatar, Time, Name, SubmitButton } from './styles';
+import api from '../../../services/api';
 
-export default function Confirm() {
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormated = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time],
+  );
+
+  async function handleConfirm() {
+    try {
+      await api.post('appointments', {
+        provider_id: provider.id,
+        date: time,
+      });
+      return navigation.navigate('Dashboard');
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
   return (
-    <View>
-      <Text>Confirm</Text>
-    </View>
+    <Background>
+      <Container>
+        <Avatar 
+          source={{
+            uri: provider.avatar ? provider.avatar 
+            : `https://api.adorable.io/avatars/40/${provider.name}.png` 
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{dateFormated}</Time>
+        <SubmitButton
+          onPress={handleConfirm}
+        >
+          Agendar
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 }
+
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirme o Horario',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={24} color="#FFF" />
+    </TouchableOpacity>
+  )
+});
